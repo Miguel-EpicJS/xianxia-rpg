ee4772b8b9c4a4d1ea5de795389bdb1c91a22af4
CodigoDeSenior <migueljnm840@gmail.com>
2022-12-24 05:27:16 -0300

    migrate documentation

--- colors.py
+++ colors.py
@@ -6,21 +6,39 @@ class Colors:
         pass 
 
     def Red(self, el):
+        """
+        Change string color to red
+        """
         return f"\33[0;91m{el}\33[0m"
 
     def Green(self, el):
+        """
+        Change string color to green
+        """
         return f"\33[0;92m{el}\33[0m"
 
     def Yellow(self, el):
+        """
+        Change string color to yellow
+        """
         return f"\33[0;93m{el}\33[0m"
 
     def Blue(self, el):
+        """
+        Change string color to blue
+        """
         return f"\33[0;49;34m{el}\33[0m"
 
     def Black(self, el):
+        """
+        Change string color to black
+        """
         return f"\33[0;49;90m{el}\33[0m"
 
     def White(self, el):
+        """
+        Change string color to white
+        """
         return f"\33[0;49;97m{el}\33[0m"
 
 
--- menu.py
+++ menu.py
@@ -1,8 +1,14 @@
 class Menu:
+    """
+    Class used to create all menu prints
+    """
     def __init(self):
         pass 
 
     def mainMenu(self):
+        """
+        Print main menu
+        """
         print("Select Your Action: ")
 
         print("(0) Quit")
