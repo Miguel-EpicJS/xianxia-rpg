4ab6cb446e22030e3c57ca13b4f8d5b338f3fb2c
CodigoDeSenior <migueljnm840@gmail.com>
2023-01-01 15:22:35 -0300

    TDD - RED/GREEN - Character

---
+++ Character.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self, name, age):
+            self.name = name
+            self.age = age
+
+    def getName(self):
+        return self.name
+    
+    def getAge(self):
+        return self.age
--- test/Character.test.py
+++ test/Character.test.py
@@ -1,9 +1,25 @@
-import unittest
+import unittest, sys
+
+from pathlib import Path
+
+path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+
+sys.path.insert(0, path)
+
+from Character import Character
+
+print(sys.path)
 
 class TestCharacterMethods(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_character_creation(self):
-        # TODO
-        pass
+        
+        testCharacter = Character("Name", 240)
+        
+        self.assertEqual("Name", testCharacter.getName())
+        self.assertEqual(240, testCharacter.getAge())
+
+if __name__ == '__main__':
+    unittest.main()
