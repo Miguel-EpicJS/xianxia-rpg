6a650db443ac57bf14c8cf7a9a06afa54be77af7
CodigoDeSenior <migueljnm840@gmail.com>
2023-01-02 15:19:17 -0300

    TDD - RED - Character

--- test/Character.test.py
+++ test/Character.test.py
@@ -15,11 +15,15 @@ class TestCharacterMethods(unittest.TestCase):
         pass
 
     def test_character_creation(self):
+        characterName = "name"
+        characterAge = 240
+        testCharacter = Character(characterName, characterAge)
         
-        testCharacter = Character("Name", 240)
-        
-        self.assertEqual("Name", testCharacter.getName())
-        self.assertEqual(240, testCharacter.getAge())
+        self.assertEqual(characterName, testCharacter.getName(), "Name should match")
+        self.assertEqual(characterAge, testCharacter.getAge(), "Age should match")
+
+        self.assertNotEqual("randomName", testCharacter.getName(), "Name should not match")
+        self.assertNotEqual(40, testCharacter.getAge(), "Age should not match")
 
 if __name__ == '__main__':
     unittest.main()
